8881d7fa8f98248108c11e441152934eaa98c3bd
Ramón <rntaboada@yahoo.com.ar>
2022-08-16 00:52:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { buildtable } from "./build_table.js";
 import { listdata } from "./list_data.js"
 
+/*
 fetch('./getData.php')
     .then(response => response.json())
     .then(data => {
@@ -8,4 +9,14 @@ fetch('./getData.php')
         buildtable(data.length)
         listdata(data, true);
     })
+*/
 
+async function getResponse() {
+    const response = await fetch(`./getData.php?offset=0&count=20`);
+    const data = await response.json();
+    return data;
+}
+
+const data = await getResponse();
+buildtable(data.length);
+listdata(data, true);
