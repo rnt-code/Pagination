5820273786e5741c1a89e3dd2386718925f43194
Ramón Taboada <rntaboada@yahoo.com.ar>
2022-09-06 21:11:12 -0300

    Cleaning
    Only the specific files are left, the example data is now in the same index.js to see the app working.

--- getData.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include('connect_db.php');
-
-    $query = "SELECT * FROM employees_73_12col";
-    $result = mysqli_query($connection, $query);
-    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    echo json_encode($data);
-?>
